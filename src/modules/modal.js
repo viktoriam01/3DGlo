e70ca282637d46034c4eb462340ad5a0b0e5b4c2
Viktoria Motoshkina <aislingdar@gmail.com>
2022-03-14 01:51:00 +0800

    исправлена анимация модального окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -7,32 +7,25 @@ const modal = () => {
    // console.log(content.getBoundingClientRect());
    // console.log(document.documentElement.clientWidth);
 
-   
-   let count = 0
-   let idInterval
-
-    
-   buttons.forEach(btn => {
+        
+  buttons.forEach(btn => {
 
       btn.addEventListener('click', () => {
          modal.style.display = 'block'
 
          if (document.documentElement.clientWidth >= 768) {
-         content.style.top = -360 + `px` 
+         content.style.transform = `translateY(-150%)` 
          flyAnimate() 
          } 
       })
    })
 
    const flyAnimate = () => {
-      count++
-      idInterval = requestAnimationFrame(flyAnimate)
-
-      if (count <= 80) {
-         content.style.top = count + `px`
+      
+      if (!content.style.transform) {
+         content.style.transform = `translateY(0)`
       }  else {
-         cancelAnimationFrame(idInterval)
-         count = 0
+         content.style.transform = ''
       }
    }
 
